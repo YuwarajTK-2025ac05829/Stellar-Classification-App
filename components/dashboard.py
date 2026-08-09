8d66f0e3e2e195964f09c5283777523c6781c285
yuwaraj <yuwaraj02@gmail.com>
2026-08-09 11:18:44 +0530

    add ipynb file

--- components/dashboard.py
+++ components/dashboard.py
@@ -24,7 +24,20 @@ def render_dashboard(selected_model):
 
     c1, c2, c3, c4 = st.columns(4)
 
-    c1.metric("Dataset", "SDSS17")
+    c1.metric("Dataset", "Stellar Classification")
+
+    c1.markdown(
+        """
+        <a href="https://www.kaggle.com/datasets/fedesoriano/stellar-classification-dataset-sdss17"
+        target="_blank">
+        🔗 Open Dataset
+        </a>
+        """,
+        unsafe_allow_html=True
+    )
+
+
+    
     c2.metric("Features", "14")
     c3.metric("Classes", "3")
     c4.metric("Model", selected_model)
